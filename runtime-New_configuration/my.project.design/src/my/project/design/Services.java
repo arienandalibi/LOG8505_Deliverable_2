86bfeac21af54cdeb8a3fe02b9a4c0f96515bcc7
arienandalibi <79378897+arienandalibi@users.noreply.github.com>
2024-11-25 22:00:36 -0500

    Added node creation tools

--- runtime-New_configuration/my.project.design/src/my/project/design/Services.java
+++ runtime-New_configuration/my.project.design/src/my/project/design/Services.java
@@ -6,10 +6,25 @@ import org.eclipse.emf.ecore.EObject;
  * The services class used by VSM.
  */
 public class Services {
-    
+    private int operationalCapabilityCounter = 1;
+    private int operationalActorCounter = 1;
+    private int operationalEntityCounter = 1;
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
+    
+    public String getOperationalCapabilityName(EObject self) {
+    	return "OperationalCapability " + operationalCapabilityCounter++;
+    }
+    
+    public String getOperationalActorName(EObject self) {
+    	return "Actor " + operationalActorCounter++;
+    }
+    
+    public String getOperationalEntityName(EObject self) {
+    	return "Entity " + operationalEntityCounter++;
+    }
+    
     public EObject myService(EObject self, String arg) {
        // TODO Auto-generated code
       return self;
